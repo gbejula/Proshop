7f143e036ba4a4943603cc6333b9e2051bb4cad3
Olugbenga Ajayi <gbejula@gmail.com>
2021-02-07 19:47:31 +0100

    custom authentication with middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from "express";
-import connectDB from "./config/db.js";
-import colors from "colors";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import dotenv from "dotenv";
+import express from 'express';
+import connectDB from './config/db.js';
+import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import dotenv from 'dotenv';
 
-import productRoutes from "./routes/productRoutes.js";
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -12,11 +13,14 @@ connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API is running");
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running');
 });
 
-app.use("/api/products", productRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 
